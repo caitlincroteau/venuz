b0cc202c21cb9e4a022edab498fb13085f2016ea
Caitlin Croteau <cjcroteau@gmail.com>
2023-02-06 14:50:01 -0800

    add background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 import logo from "./components/images/venuz-logo-star.png";
+import background from "./images/venuz_background.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" 
+    style={{backgroundImage: `url(${background})`,
+    height:'100vh',
+    marginTop:'-70px',
+    fontSize:'50px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: '#816bfe'
+    }}>
       <h1>Welcome to Venuz!</h1>
     </div>
   );
