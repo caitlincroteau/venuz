7e1cdb0c2226f54b4fd60e1155977ecfdecb536a
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-24 14:55:36 -0700

    add map component

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 const MapContainer = () => {
-  
-  const mapStyles = {        
+  const mapStyles = {
     height: "50vh",
-    width: "50%"};
-  
+    width: "50%",
+  };
+
   const defaultCenter = {
-    lat: 41.3851, lng: 2.1734
-  }
-  
+    lat: 41.3851,
+    lng: 2.1734,
+  };
+
   return (
-     <LoadScript
-       googleMapsApiKey= {process.env.GOOGLE_MAPS_API}>
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={13}
-          center={defaultCenter}
-        />
-     </LoadScript>
-  )
-}
+    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+      />
+    </LoadScript>
+  );
+};
 
 export default MapContainer;
