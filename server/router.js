892cb2cf714ebe253d2ca87c6d368d7dd60b0564
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-24 15:33:53 -0700

    add router file

---
+++ server/router.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const {
+  createVenue,
+  getVenues,
+  getVenue,
+  updateVenue,
+  deleteVenue,
+} = require("./controllers/venueController");
+
+router.get("/", (req, res) => {
+  res.send("My router");
+});
+
+//CREATE add one
+router.post("/api/venue", createVenue);
+
+//READ all
+router.get("/api/venues", getVenues);
+
+//READ one
+router.get("/api/venues/:id", getVenue);
+
+//UPDATE one
+router.put("/api/venues/:id", updateVenue);
+
+//DELETE one
+router.delete("/api/venues/:id", deleteVenue);
+
+module.exports = { router };
