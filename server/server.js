9342d425975b250c92a018855727c8d2665717b8
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-24 15:08:44 -0700

    update server file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,49 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-//connect to Mongo atlas database
+// const db = mongoose.connection;
+
+// //get notification of errors
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+//requirements
+const express = require("express");
+const morgan = require("morgan");
+const { router } = require("./router");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-mongoose.connect(process.env.REACT_APP_MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+//setup
+const app = express();
+const port = 3000;
+dotenv.config();
+
+//mongo database connection
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//middleware
+app.use(morgan("dev"));
+app.use(cors());
+//so app can access json data
+app.use(express.json());
+//so app can access form input data
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+//listener
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
 
-const db = mongoose.connection;
-
-//get notification of errors
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+//video
+//https://www.youtube.com/watch?v=9OfL9H6AmhQ&ab_channel=Devtamin
