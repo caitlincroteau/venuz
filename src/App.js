6bca4ea8388df7b51a8737529cfdc33144b915cd
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-26 23:26:00 -0700

    add venues component and clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import "./App.css";
 import logo from "./components/images/venuz-logo-star.png";
 import background from "./images/venuz_background.png";
-import Map from "./components/map";
-import Venue from "./components/venues";
-
+import useData from "./components/useData";
+import Venues from "./components/Venues";
 
 function App() {
+  const { venues, setVenues } = useData();
+
   return (
-    <div className="App" 
-    style={{backgroundImage: `url(${background})`,
-    height:'100vh',
-    marginTop:'-70px',
-    fontSize:'50px',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor: '#816bfe'
-    }}>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        height: "100vh",
+        marginTop: "-70px",
+        fontSize: "50px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#816bfe",
+      }}
+    >
       <h1>Welcome to Venuz!</h1>
-      <Venue />
-      {/* <Map /> */}
+      <Venues venues={venues} />
     </div>
   );
 }
