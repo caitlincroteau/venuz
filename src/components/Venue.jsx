a83e54ff64b86a222df7c1b24548163021dcc172
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-13 23:04:44 -0700

    refactor venue(s) components to remove country

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -1,10 +1,8 @@
-import Venues from "./Venues";
 
 export default function Venue(props) {
   return (
     <div>
       <ul>{props.name}</ul>
-      <ul>{props.country}</ul>
       <ul>{props.capacity}</ul>
     </div>
   );
--- src/components/venues.jsx
+++ src/components/venues.jsx
@@ -1,15 +1,9 @@
 import Venue from "./Venue";
+//lists all the venues
 
 export default function Venues(props) {
   const venues = props.venues.map((venue) => {
-    return (
-      <Venue
-        key={venue.id}
-        name={venue.name}
-        country={venue.country}
-        capacity={venue.capacity}
-      />
-    );
+    return <Venue key={venue.id} name={venue.name} capacity={venue.capacity} />;
   });
 
   return <ul>{venues}</ul>;
