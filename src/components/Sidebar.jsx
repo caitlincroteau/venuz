fad438a5e8dd2ac09058db4dc18943acd18d9ee2
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-18 16:32:21 -0700

    clean up sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,31 +4,12 @@ import Venue from "./Venue.jsx";
 import { AppContext } from "../Context";
 
 export default function Sidebar(props) {
-  const { activeMarker, setActiveMarker } = useContext(AppContext)
-  const { venuesDetails } = useData();
-  // console.log(typeof activeMarker)
-  console.log(activeMarker)
+  const { activeMarker, setActiveMarker } = useContext(AppContext);
 
-  // const showVenueDetails = () => {
-
-  // }
-
-  // return (
-  //   <>
-  //     <h1>Sidebar</h1>
-  //     <div>
-  //     {venuesDetails.map((venue) => {
-  //       return (venue)
-  //     })
-  //    }
-  //    </div>
-  //   </>
-  // );
   return (
     <>
       <h1>Sidebar</h1>
-      {activeMarker && (<div>name: {activeMarker.title}</div>)}
-      
+      {activeMarker && <div>name: {activeMarker.title}</div>}
     </>
   );
 }
