6926ea75c7a82c191008ded7b83489295eb7d02a
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-18 12:34:50 -0700

    add venue componenet to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import useData from "./useData.jsx";
+import Venue from "./Venue.jsx";
 
 export default function Sidebar(props) {
-  const [activeVenue, setActiveVenue] = useState()
+  const [activeVenue, setActiveVenue] = useState();
   const { venuesDetails } = useData();
 
   // const showVenueDetails = () => {
-    
+
   // }
 
+  // return (
+  //   <>
+  //     <h1>Sidebar</h1>
+  //     <div>
+  //     {venuesDetails.map((venue) => {
+  //       return (venue)
+  //     })
+  //    }
+  //    </div>
+  //   </>
+  // );
   return (
     <>
       <h1>Sidebar</h1>
-      <div> 
-      {venuesDetails.map((venue) => {
-        return (venue)
-      })
-     }
-     </div>
+      <Venue></Venue>
     </>
   );
 }
