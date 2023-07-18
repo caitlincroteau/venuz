11e5f2506c8c53e04fcf37e87921c85797e8d7a2
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-18 14:00:05 -0700

    add context file

---
+++ src/Context.js
@@ -0,0 +1,10 @@
+import { createContext } from "react"
+
+export const AppContext = createContext(null);
+
+// const MarkerContextProvider = ({children}) => (
+//   <MarkerContext.Provider value={useState(null)}>
+//     {children}
+//   </MarkerContext.Provider>
+// );
+
