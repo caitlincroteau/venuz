cfeaa2376adfb136a80e8f77e03ad63b8c155d3c
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-24 17:54:03 -0700

    use activeVenue data to show content in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,33 +4,14 @@ import Venue from "./Venue.jsx";
 import { AppContext } from "../Context";
 
 export default function Sidebar(props) {
-  const { activeMarker, venues } = useContext(AppContext);
-  const [activeVenue, setActiveVenue] = useState({});
-
-  // const showActiveVenue = () => {
-  //     for (let venue in venues) {
-  //       if (activeMarker.title === venues[venue].name) {
-  //         // setActiveVenue(venues[venue]);
-  //         console.log(activeVenue)
-          
-  //       } 
-  //     }
-  //   return (<Venue name={activeVenue.title}></Venue>);
-  // };
-  const matchVenueToMarker = () => {
-    if(activeMarker) {
-      for(let venue in venues) {
-        if(venues[venue]["name"] === activeMarker.title) {
-          setActiveVenue(venues[venue])
-        }
-      }
-
-    }
-    
-    console.log(activeVenue)
-  }
-
-  matchVenueToMarker()
+  const {
+    activeMarker,
+    setActiveMarker,
+    activeVenue,
+    setActiveVenue,
+    markersList,
+    venuesList,
+  } = useContext(AppContext);
 
   useEffect(() => {
     console.log(activeVenue);
@@ -39,8 +20,15 @@ export default function Sidebar(props) {
   return (
     <>
       <h1>Sidebar</h1>
+      {activeVenue && activeVenue.name}
       {/* {activeMarker && <div>name: {venueDetails.props[activeMarker.title]}</div>} */}
-     
+      {/* {activeMarker && (
+        <Venue
+          name={activeVenue.name}
+          capacity={activeVenue.capacity}
+          facility={activeVenue.facility}
+        />
+      )} */}
     </>
   );
 }
