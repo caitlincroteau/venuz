26875e9155ee3a4f78ce4b8840cef1153d4a7546
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-24 18:01:04 -0700

    clean up code and remove useData imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-// import logo from "./components/images/venuz-logo-star.png";
-import background from "./images/venuz_background.png";
-import useData from "./components/useData";
-import Venues from "./components/Venues";
 import MapContainer from "./components/MapContainer";
 
 function App() {
-  const { venues, setVenues } = useData();
-
   return (
-    <div
- 
-    >
-      {/* <h1>Welcome to Venuz!</h1> */}
-      {/* displays all venues at top of page */}
-      {/* <Venues venues={venues} /> */}
-
+    <div>
       <MapContainer />
     </div>
   );
