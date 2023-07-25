1aedca160fa5145f4bfc9c187ea4262eb457e793
Caitlin Croteau <cjcroteau@gmail.com>
2023-07-24 17:53:29 -0700

    replace activeMarker with activeVenue in useEffect

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -53,7 +53,7 @@ export default function Map(props) {
 
   useEffect(() => {
     // console.log(activeMarker);
-  }, [activeMarker]);
+  }, [activeVenue]);
 
   const handleClick = (marker) => {
     // setActiveMarker(marker);
